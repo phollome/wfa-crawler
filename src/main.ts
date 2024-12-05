23712c730f7727305d20583cd10d10089278f0df
Peter Holló <hello@songsforthe.dev>
2024-12-05 16:56:05 +0100

    Get description from given episode url

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,55 @@
-// For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
+import { PlaywrightCrawler, Dataset } from "crawlee";
+import { readFile } from "node:fs/promises";
 
-import { router } from './routes.js';
+let urlsFileContent: string;
+let urls: string[];
 
-const startUrls = ['https://crawlee.dev'];
+try {
+  urlsFileContent = await readFile("urls.txt", "utf-8");
+  urls = urlsFileContent.split("\n");
+} catch (error) {
+  console.error("Error reading URLs", error);
+  process.exit(1);
+}
+
+const result: {
+  href: string;
+  title: string;
+  description: string;
+  pubDate: string;
+}[] = [];
 
 const crawler = new PlaywrightCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+  requestHandler: async (args) => {
+    const { page, request } = args;
+
+    console.log("Requesting", request.url);
+
+    const title = await page.getByRole("heading", { level: 1 }).innerText();
+    const description = await page.locator(".episode-show-notes").innerText();
+
+    const dateString = await page.getByRole("time").locator("//span").getAttribute("data-timestamp");
+
+    if (dateString === null) {
+      throw new Error("Date string not found");
+    }
+    
+    // Convert the timestamp to milliseconds
+    const timestamp = parseInt(dateString) * 1000;
+    
+    const date = new Date(timestamp).toISOString();
+
+    result.push({
+      href: request.url,
+      title,
+      description,
+      pubDate: date,
+    });
+  },
 });
 
-await crawler.run(startUrls);
+await crawler.addRequests(urls);
+await crawler.run();
+
+const dataset = await Dataset.open("episodes");
+await dataset.pushData({ data: result });
