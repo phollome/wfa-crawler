7cc0e87bd41a921a9685ffe3206bd3f4bf70f47c
Peter Holló <hello@songsforthe.dev>
2024-12-05 20:55:47 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,107 @@
-import { PlaywrightCrawler, Dataset } from "crawlee";
-import { readFile } from "node:fs/promises";
+import OpenAI from "openai";
 
-let urlsFileContent: string;
-let urls: string[];
+import { Dataset, PlaywrightCrawler } from "crawlee";
+import dotenv from "dotenv";
+import { readFile, readdir } from "node:fs/promises";
+import { z } from "zod";
+import { zodResponseFormat } from "openai/helpers/zod";
 
-try {
-  urlsFileContent = await readFile("urls.txt", "utf-8");
-  urls = urlsFileContent.split("\n");
-} catch (error) {
-  console.error("Error reading URLs", error);
-  process.exit(1);
-}
+dotenv.config();
 
-const result: {
+// let urlsFileContent: string;
+// let urls: string[];
+
+// try {
+//   urlsFileContent = await readFile("urls.txt", "utf-8");
+//   urls = urlsFileContent.split("\n");
+// } catch (error) {
+//   console.error("Error reading URLs", error);
+//   process.exit(1);
+// }
+
+type Episode = {
   href: string;
   title: string;
   description: string;
   pubDate: string;
-}[] = [];
+};
 
-const crawler = new PlaywrightCrawler({
-  requestHandler: async (args) => {
-    const { page, request } = args;
+// const result: Episode[] = [];
 
-    console.log("Requesting", request.url);
+// const crawler = new PlaywrightCrawler({
+//   requestHandler: async (args) => {
+//     const { page, request } = args;
 
-    const title = await page.getByRole("heading", { level: 1 }).innerText();
-    const description = await page.locator(".episode-show-notes").innerText();
+//     console.log("Requesting", request.url);
 
-    const dateString = await page.getByRole("time").locator("//span").getAttribute("data-timestamp");
+//     const title = await page.getByRole("heading", { level: 1 }).innerText();
+//     const description = await page.locator(".episode-show-notes").innerText();
 
-    if (dateString === null) {
-      throw new Error("Date string not found");
-    }
-    
-    // Convert the timestamp to milliseconds
-    const timestamp = parseInt(dateString) * 1000;
-    
-    const date = new Date(timestamp).toISOString();
+//     const dateString = await page
+//       .getByRole("time")
+//       .locator("//span")
+//       .getAttribute("data-timestamp");
 
-    result.push({
-      href: request.url,
-      title,
-      description,
-      pubDate: date,
-    });
-  },
+//     if (dateString === null) {
+//       throw new Error("Date string not found");
+//     }
+
+//     // Convert the timestamp to milliseconds
+//     const timestamp = parseInt(dateString) * 1000;
+
+//     const date = new Date(timestamp).toISOString();
+
+//     result.push({
+//       href: request.url,
+//       title,
+//       description,
+//       pubDate: date,
+//     });
+//   },
+// });
+
+// await crawler.addRequests(urls);
+// await crawler.run();
+
+const datasetName = "episodes";
+
+// const dataset = await Dataset.open(datasetName);
+// await dataset.pushData({ data: result });
+
+const datasetFiles = await readdir(`./storage/datasets/${datasetName}`);
+if (datasetFiles.length === 0) {
+  console.error("No data found in the dataset");
+  process.exit(1);
+}
+
+const file = datasetFiles[datasetFiles.length - 1];
+const content = await readFile(
+  `./storage/datasets/${datasetName}/${file}`,
+  "utf-8"
+);
+const parsedContent = JSON.parse(content) as { data: Episode[] };
+
+const openai = new OpenAI();
+const referencesSchema = z.object({
+  title: z.string(),
+  author: z.string(),
+  publisher: z.string(),
+  href: z.string().optional(),
 });
+const config = {
+  model: "gpt-4o-mini",
+  response_format: zodResponseFormat(referencesSchema, "reference"),
+}
 
-await crawler.addRequests(urls);
-await crawler.run();
 
-const dataset = await Dataset.open("episodes");
-await dataset.pushData({ data: result });
+for (const episode of parsedContent.data) {
+  const completions = await openai.beta.chat.completions.parse({...config, messages: [{
+    role: "user",
+    content: [{
+      type: "text",
+      text: `Extract bibliographic references out of following text and output only the references converted into the given structure": ${episode.description} If it's an online resource use domain as publisher. Output only the references.`
+    }],
+  }]})
+  console.log(completions)
+}
+
